1c6cc0a3e8884fa9047b1f0338c861f285b07971
narcis13 <narcis75@gmail.com>
2024-05-30 13:50:53 +0300

    un poco

---
+++ server/api/iam/doodads/index.ts
@@ -0,0 +1,10 @@
+/** This is an example endpoint and can be deleted.
+ * If you delete it, also delete ~~iam/mvc/doodads directory
+ */
+
+import doodadsController from "~~/iam/mvc/doodads/controller";
+
+export default defineEventHandler(async (event) => {
+    console.log('index.ts')
+  return doodadsController(event);
+});
