4e8d6932037b97e8a55b446a5d526cbd9b48eeee
narcis13 <narcis75@gmail.com>
2024-05-31 14:19:27 +0300

    cu ai

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  runtimeConfig:{
+    openai_key:process.env.openai_key,
+    public:{
+     // placeHost:process.env.NUXT_PLACE_HOST,
+    }
+    
+  },
   modules: ['@pinia/nuxt', "nuxt-quasar-ui", "@nuxtjs/supabase"]
 })
--- server/api/ai/test.get.js
+++ server/api/ai/test.get.js
@@ -1,8 +1,18 @@
+import OpenAI from "openai"
 
 
-export default defineEventHandler(async (event) => {
-   
-    return {
-      message: 'success',
-    };
+//env("openai_key")
+const config = useRuntimeConfig();
+const openai = new OpenAI({
+  apiKey: config.openai_key,
+})
+
+export default defineEventHandler(async (event) =>{
+  
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a very talented marketer. Come up please with 10 ideas for content about a new AI app for accountants!" }],
+    model: "gpt-4o",
   });
+
+    return {text:completion.choices[0].message.content}
+  })
