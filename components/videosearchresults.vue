570854c5ac8ca6d97c3966aec08a136ef7e450c2
narcis13 <narcis75@gmail.com>
2024-06-11 15:04:55 +0300

    ...

---
+++ components/videosearchresults.vue
@@ -0,0 +1,77 @@
+<script setup>
+const   tab= ref('keywords')
+const  splitterModel= ref(20)
+const props = defineProps({
+    researchdata:Object
+})
+
+
+console.log('videoresearch',props.researchdata)
+</script>
+
+
+
+
+
+<template>
+    <div>
+      <q-splitter
+        v-model="splitterModel"
+        style="height: 350px"
+      >
+  
+        <template v-slot:before>
+          <q-tabs
+            v-model="tab"
+            vertical
+            class="text-teal"
+          >
+            <q-tab name="keywords" icon="mail" label="Keywords" />
+            <q-tab name="titles" icon="alarm" label="Titles" />
+            <q-tab name="description" icon="movie" label="Description" />
+            <q-tab name="transcriptions" icon="movie" label="Transcriptions" />
+          </q-tabs>
+        </template>
+  
+        <template v-slot:after>
+          <q-tab-panels
+            v-model="tab"
+            animated
+            swipeable
+            vertical
+            transition-prev="jump-up"
+            transition-next="jump-up"
+          >
+            <q-tab-panel name="keywords">
+              <div class="text-h4 q-mb-md">Keywords</div>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            </q-tab-panel>
+  
+            <q-tab-panel name="titles">
+              <div class="text-h4 q-mb-md">Titles</div>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            </q-tab-panel>
+  
+            <q-tab-panel name="description">
+              <div class="text-h4 q-mb-md">Description</div>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            </q-tab-panel>
+
+            <q-tab-panel name="transcriptions">
+              <div class="text-h4 q-mb-md">Transcriptions</div>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+            </q-tab-panel>
+
+
+          </q-tab-panels>
+        </template>
+  
+      </q-splitter>
+    </div>
+  </template>
--- pages/research/index.vue
+++ pages/research/index.vue
@@ -2,7 +2,9 @@
 import { useQuasar } from 'quasar'
 const $q = useQuasar()
 
-
+let  researchdata =reactive({
+    data:{}
+})
 const model=ref("llama3")
 const description=ref("You are going to create extremely good and captivating videos about ")
 const topic = ref()
@@ -25,6 +27,7 @@ async function research(){
           description:description.value       },
       });
       $q.loading.hide()
+      researchdata.data=response.data
      console.log(response) 
 }
 </script>
@@ -40,7 +43,11 @@ async function research(){
             <q-input class="q-mt-sm" outlined v-model="topic" label="Topic" />
             <q-input class="q-mt-sm" outlined v-model="description" type="textarea" label="Description" />
             <q-input class="q-mt-sm q-mb-md" outlined v-model="model" label="AI Model" />
-            <q-btn color="grey-4" text-color="purple" glossy unelevated icon="camera_enhance" @click="research" label="Research" />
+            <div class="flex flex-center">
+                <q-btn class="q-mb-md" color="grey-4" text-color="purple" glossy unelevated icon="camera_enhance" @click="research" label="Research" style="max-width: 200px;"/>
+            </div>
+           
+            <videosearchresults :researchdata="researchdata"/>
         </div>
 
     </q-page>
