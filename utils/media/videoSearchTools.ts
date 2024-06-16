754d2ddfcb8a07e68a12aa564e4f15bef8440675
Narcis13 <Narcis75@gmail.com>
2024-06-16 18:29:49 +0300

    reparat

--- utils/media/videoSearchTools.ts
+++ utils/media/videoSearchTools.ts
@@ -6,6 +6,7 @@ type VideoSearchResults = {
   channelId: string;
   channelTitle: string;
   daysSincePublished: number;
+  duration?:any;
 }
 
 type VideoDetails = {
@@ -18,6 +19,7 @@ type VideoDetails = {
   score: number;
   videoUrl:string;
   id:string;
+  duration?:any;
 }
 
 
@@ -34,10 +36,12 @@ export const videoSearchTool = async (topic: string, maxResults: number = 35): P
 
   if (data.items) {
     for (const item of data.items) {
+      //console.log(item)
       const id = item.id.videoId;
       const title = item.snippet.title;
       const channelId = item.snippet.channelId;
       const channelTitle = item.snippet.channelTitle;
+    //  const duration = item.contentDetails.duration;
       const daysSincePublished = Math.floor((new Date().getTime() - new Date(item.snippet.publishedAt.slice(0, 10)).getTime())/(1000*60*60*24)); 
 
       results.push({
@@ -46,6 +50,7 @@ export const videoSearchTool = async (topic: string, maxResults: number = 35): P
         channelId,
         channelTitle,
         daysSincePublished,
+       // duration
       });
     }
   }
@@ -55,7 +60,7 @@ export const videoSearchTool = async (topic: string, maxResults: number = 35): P
 
 export const videoDetailsTool = async (video: VideoSearchResults): Promise<VideoDetails> => {
   const apiKey = useRuntimeConfig().youtube_api_key;
-  const videoUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics&id=${video.id}&key=${apiKey}`;
+  const videoUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet,statistics,contentDetails&id=${video.id}&key=${apiKey}`;
   const channelUrl = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${video.channelId}&key=${apiKey}`;
 
   const videoResponse = await fetch(videoUrl);
@@ -63,13 +68,13 @@ export const videoDetailsTool = async (video: VideoSearchResults): Promise<Video
 
   const videoData = await videoResponse.json();
   const channelData = await channelResponse.json();
-
+ // console.log('Stats',videoData.items[0].contentDetails?.duration.replace(/(^PT|S$)/g, "").split(/[^\d]/).map((item) => item.length < 2 ? "0" + item : item).join(":").replace(/^0/, ""))
   const title = videoData.items[0].snippet.title;
   const viewCount = (videoData.items[0].statistics.viewCount as number);
   const likeCount = videoData.items[0].statistics.likeCount;
   const commentCount = videoData.items[0].statistics.commentCount;
   const subscriberCount = channelData.items[0].statistics.subscriberCount;
-
+  const duration = video.duration;
   const videoString = `   - Title: ${title}\n   - Channel: ${video.channelTitle}\n   - View Count: ${Number(viewCount).toLocaleString()}\n   - Days Since Published: ${video.daysSincePublished}\n   - Likes: ${Number(likeCount).toLocaleString()}\n   - Subscriber Count: ${Number(subscriberCount).toLocaleString('en-US')}\n   - Video URL: https://www.youtube.com/watch?v=${video.id}\n`;
 
   // const score = (viewCount / video.daysSincePublished / subscriberCount) + (likeCount * 10) + (commentCount * 100);
@@ -84,7 +89,8 @@ export const videoDetailsTool = async (video: VideoSearchResults): Promise<Video
     subscriberCount,
     score,
     videoUrl,
-    id:video.id
+    id:video.id,
+    duration
   };
 }
 
