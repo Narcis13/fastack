6d64ae4898fd4b53c88e3985c81f1aebf7eb5aaf
Narcis13 <Narcis75@gmail.com>
2024-06-18 19:10:02 +0300

    putin

--- components/videoscriptgenesis.vue
+++ components/videoscriptgenesis.vue
@@ -12,6 +12,46 @@ const description=ref(props.researchdata.data.newdescription)
 const tone=ref(props.researchdata.data.summary.tone)
 const audience = ref('young and mid-age, smart and curious eager to grow as a person, slightly more women than men')
 const model=ref('llama-3-sonar-large-32k-online')
+/*
+'m going to provide you with some frameworks and your task will be to understand and write hooks for tweets using these frameworks and examples.
+Frameworks There are 2 main goals you're looking to achieve when writing a hook:
+Get the reader to stop scrolling
+Get the reader to click to read more 5 elements of writing an effective headline:
+Be CLEAR not Clever
+Specify the WHO
+Specify the WHAT
+Specify the WHY
+
+See exam
+Instructions (optional)
+Specity the WHY
+Twist The Knife
+6 proven ways to write an engaging first sentence:
+Open with 1 strong declarative sentence.
+Open with a thought-provoking question.
+Open with a controversial opinion.
+Open with a moment in time.
+Open with a vulnerable statement.
+Open with a weird unique insight.
+Viral Tweet Thread Example #1
+Principles this thread uses & why they worked:
+Strong declarative sentence + controversial opinion opener.
+"If you use it right, Twitter is the most powerful platform in
+
+See example
+Instructions (optional)
+Open with a vulnerable statement.
+Open with a weird unique insight.
+Viral Tweet Thread Example #1.
+Principles this thread uses & why they worked:
+Strong deciarative sentence + controversial opinion opener.
+"I you use it right, Twitter is the most powerful platform in the world." This sentence makes a bold claim that is somewhat controversial. Most readers will disagree with that first sentence, which will cause them to keep reading.
+Twist the knife. "But Twitter does a horrible job showing you Its advanced features. you probably know nothing about." This sentence tells the reeder: "you're missing out on some awesome stuffi But it's not your fault, and i'm here to help."
+
+The first sentence hooked them and the second sentence got them wanting more of what feels like "insider info." Clear, not clever. "Here are 10 of them you probably know nothing about." As clear as it gets - I am making a promise to the reader that what follows are 10 features they don't know about (scratching the itch and solving the problem I laid out in the 2nd sentence):
+This is a classic "insider info secrets" listicle you can use time and time again. Tell the reader you have some insider Into (and how the world has conspired against them to keep them from knowing about it). Then ease their pain by giving them the insider info in a concise listicle format.
+
+*/ 
 const hookprompt=ref(`
 You are an AI language model tasked with generating engaging hooks for YouTube videos. You will receive a title, description, tone, and target audience for a YouTube video. Your job is to create a captivating hook that will grab the viewers' attention and entice them to watch the video. 
 
@@ -161,7 +201,8 @@ Example of an AI Response:
 
 `)
 const videoscriptprompt=ref(`
-You are an AI language model tasked with generating comprehensive content for YouTube videos. You will receive a title, description, tone, target audience, and a detailed outline for a YouTube video. Your job is to create detailed content for each subtopic under each key point in the outline, ensuring the entire content has more than 500 words.
+You are an AI language model tasked with generating comprehensive content for YouTube videos. You will receive a title, description, tone, target audience, and a detailed outline for a YouTube video.
+Your job is to create detailed content for each subtopic under each key point in the outline, ensuring that each subtopic content  has more than 200 words.
 
 Ensure the content is engaging, relevant to the provided information, and suited to the given tone and audience.
 
